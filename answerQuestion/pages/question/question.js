351e71ddb4415f0ffff5aaafaed646954ef76b71
wuqian <1307144731@qq.com>
2018-02-02 23:46:21 +0800

    增加新的文件

--- answerQuestion/pages/question/question.js
+++ answerQuestion/pages/question/question.js
@@ -5,27 +5,20 @@ const questionList = require('../../utils/questionList.js')
 
 Page({
   data: {
-    questionIndex: 0,
+    questionIndex: 1,
     questionList: null,
   },
   onLoad: function (options) {
     console.log(options);
-  },
-  // 点击答案触发 
-  myAnswer(event) {
-    console.log(event);
-    this.setData({
-      questionIndex: this.data.questionIndex + 1,
-    })
-  },
-  showQueation() {
-    this.setData({
-      questionIndex: this.data.questionIndex + 1,
-    })
-    console.log('a', this.data.questionIndex);
-    console.log('list', questionList.myList);
     this.setData({
       questionList: questionList.myList,
     })
+  },
+  // 点击答案触发 
+  NextQueation(e) {
+    console.log(99999999997, e);
+    if (e.currentTarget.dataset.status) {
+      console.log('答对了');
+    }
   },
 })
